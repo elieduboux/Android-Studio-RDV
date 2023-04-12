abc8742540fdafd47df835e7c479f5756535c744
gyoza <minielie8@gmail.com>
2023-04-12 18:44:11 +0200

    music done

--- app/src/main/java/com/example/projet/Settings.java
+++ app/src/main/java/com/example/projet/Settings.java
@@ -1,5 +1,7 @@
 package com.example.projet;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,7 +19,7 @@ public class Settings extends AppCompatActivity {
 
     Switch swDarkMode;
     ImageView btPlayMusic;
-    boolean isClicked;
+    boolean isPlaying;
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
@@ -27,13 +29,23 @@ public class Settings extends AppCompatActivity {
             swDarkMode.setChecked(true);
 
         btPlayMusic = findViewById(R.id.settings_music_bt);
-
+        isPlaying = isMyServiceRunning(MusicService.class);
+        if(isPlaying)
+            btPlayMusic.setImageResource(R.drawable.stop);
     }
 
     public void playAndPauseMusic(View view){
-
-        Intent serviceIntent= new Intent(getApplicationContext(),MusicService.class);
-        startService(serviceIntent);
+        if (isPlaying) {
+            isPlaying = false;
+            btPlayMusic.setImageResource(R.drawable.play);
+            stopService(new Intent(this, MusicService.class));
+        }
+        else {
+            isPlaying = true;
+            btPlayMusic.setImageResource(R.drawable.stop);
+            Intent serviceIntent = new Intent(getApplicationContext(), MusicService.class);
+            startService(serviceIntent);
+        }
     }
 
     public void switchTheme(View view){
@@ -41,7 +53,17 @@ public class Settings extends AppCompatActivity {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         else
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
 
+    @SuppressWarnings("deprecation")
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
